710db3b934d03309af57deda0bae883557a4803c
Alex R Owens <alexowens12@Alexs-MacBook-Pro.local>
2017-01-10 08:18:23 -0700

    Implemented destry feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,5 +40,17 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    #Going to perform the look up
+    @portfolio_item = Portfolio.find(params[:id])
+    #Going to destroy the record
+    @portfolio_item.destroy
+    #Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was successfully removed.' }
+    end
+  end
+
+
 
 end
