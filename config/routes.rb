ebe6c0030b3b03d2869fe46886fa7080de61580a
Alex R Owens <alexowens12@Alexs-MacBook-Pro.local>
2017-01-20 10:15:15 -0700

    Implemented Custom Scopes for Portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
 
   root to: 'pages#home'
 end
