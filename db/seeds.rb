4982018be0b410616b274135a83f3658d3bdadb3
Alex R Owens <alexowens12@Alexs-MacBook-Pro.local>
2017-01-23 10:21:19 -0700

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,4 +45,11 @@ end
   )
 end
 
-puts "9 Portoflio items created"
+puts "9 Portfolio items created"
+
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+    )
+end
+  puts "3 Technologies created"
